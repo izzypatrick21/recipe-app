621aa550348941f288ca226c3c1124db38683a5f
Patrick <patrick21isaiah@gmail.com>
2023-10-22 12:02:45 +0100

    comments

--- mainv2.py
+++ mainv2.py
@@ -1,7 +1,9 @@
+# Equivalent to the script in main.py but here I used class
 import tkinter as tk
 from PIL import ImageTk, Image
 import sqlite3
 from numpy import random
+
 
 class RecipePicker:
     def __init__(self, root):
@@ -15,15 +17,15 @@ class RecipePicker:
         self.dark_color = "#000000"
 
         self.recipe_logo = ImageTk.PhotoImage(file="assets/recipe_logo.png")
-        self.recipe_logo_bottom = ImageTk.PhotoImage(file="assets/recipe_logo_bottom.png")
+        self.recipe_logo_bottom = ImageTk.PhotoImage(
+            file="assets/recipe_logo_bottom.png"
+        )
 
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
 
-
         self.mainFrame = tk.Frame(self.root, bg=self.bg_color)
         self.recipeFrame = tk.Frame(self.root, bg=self.bg_color)
-
 
         for frame in (self.mainFrame, self.recipeFrame):
             frame.grid(row=0, column=0, sticky="nesw")
@@ -45,7 +47,7 @@ class RecipePicker:
         table_name = all_tables[idx][1]
         cursor.execute("SELECT * FROM " + table_name + ";")
         table_records = cursor.fetchall()
-  
+
         connection.close()
         return table_name, table_records
 
@@ -53,7 +55,9 @@ class RecipePicker:
         title = table_name[:-6]
         title = "".join([char if char.islower() else " " + char for char in title])
 
-        ingredients = [f"{record[2]} {record[3]} of {record[1]}" for record in table_records]
+        ingredients = [
+            f"{record[2]} {record[3]} of {record[1]}" for record in table_records
+        ]
         return title, ingredients
 
     def load_frame1(self):
@@ -63,10 +67,24 @@ class RecipePicker:
         logo_widget = tk.Label(self.mainFrame, bg=self.bg_color, image=self.recipe_logo)
         logo_widget.pack()
 
-        guide_text = tk.Label(self.mainFrame, text="Ready for your random recipe?", bg=self.bg_color, fg=self.fg_color, font=("TkMenuFont", 14))
+        guide_text = tk.Label(
+            self.mainFrame,
+            text="Ready for your random recipe?",
+            bg=self.bg_color,
+            fg=self.fg_color,
+            font=("TkMenuFont", 14),
+        )
         guide_text.pack()
 
-        shuffle_button = tk.Button(self.mainFrame, text="SHUFFLE", cursor="hand2", font=("TkHeadingFont", 18), bg=self.fg_color, fg=self.dark_color, command=self.load_frame2)
+        shuffle_button = tk.Button(
+            self.mainFrame,
+            text="SHUFFLE",
+            cursor="hand2",
+            font=("TkHeadingFont", 18),
+            bg=self.fg_color,
+            fg=self.dark_color,
+            command=self.load_frame2,
+        )
         shuffle_button.pack(pady=20)
 
     def load_frame2(self):
@@ -76,16 +94,38 @@ class RecipePicker:
         table_name, table_records = self.fetch_db()
         title, ingredients = self.pre_process(table_name, table_records)
 
-        logo_widget = tk.Label(self.recipeFrame, bg=self.bg_color, image=self.recipe_logo_bottom)
+        logo_widget = tk.Label(
+            self.recipeFrame, bg=self.bg_color, image=self.recipe_logo_bottom
+        )
         logo_widget.pack(pady=20)
 
-        title_wg = tk.Label(self.recipeFrame, text=title, bg=self.bg_color, fg=self.fg_color, font=("TkHeadingFont", 20))
+        title_wg = tk.Label(
+            self.recipeFrame,
+            text=title,
+            bg=self.bg_color,
+            fg=self.fg_color,
+            font=("TkHeadingFont", 20),
+        )
         title_wg.pack(pady=25)
 
         for ingredient in ingredients:
-            tk.Label(self.recipeFrame, text=ingredient, bg="#1f0036", fg=self.fg_color, font=("TkMenuFont", 12)).pack(fill="both", padx=20)
+            tk.Label(
+                self.recipeFrame,
+                text=ingredient,
+                bg="#1f0036",
+                fg=self.fg_color,
+                font=("TkMenuFont", 12),
+            ).pack(fill="both", padx=20)
 
-        back_btn = tk.Button(self.recipeFrame, text="Back", cursor="hand2", font=("TkHeadingFont", 18), bg=self.fg_color, fg=self.dark_color, command=self.load_frame1)
+        back_btn = tk.Button(
+            self.recipeFrame,
+            text="Back",
+            cursor="hand2",
+            font=("TkHeadingFont", 18),
+            bg=self.fg_color,
+            fg=self.dark_color,
+            command=self.load_frame1,
+        )
         back_btn.pack(pady=20)
 
 
